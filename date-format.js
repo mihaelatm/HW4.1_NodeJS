8db215056f9cc48eb29236d04704649546fa5f70
Mihaela Talmazan <talmazanmihaela@gmail.com>
2024-11-04 20:58:11 +0100

    Added changes

--- date-format.js
+++ date-format.js
@@ -2,10 +2,10 @@ import moment from "moment";
 
 const now = moment();
 
-const formatDate1 = now.format("DD-MM-YYYY");
-const formatDate2 = now.format("MMM Do YY");
-const formatDate3 = now.format("dddd");
+const formatDate1 = now.format("DD-MM-YYYY h:mm:ss");
+const formatDate2 = now.format("MMM Do YY h:mm:ss");
+const formatDate3 = now.format("dddd h:mm:ss");
 
 console.log("Current Date: ", formatDate1);
 console.log("Current Date: ", formatDate2);
-console.log("Current Date: ", formatDate3);
+console.log("Current Day: ", formatDate3);
